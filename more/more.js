758b4850b4aef46cafa0a863a26740aa8dd54686
armored-dragon <publicmail@armoreddragon.com>
2025-06-02 07:29:06 -0500

    Removed now unneeded todos.

--- more/more.js
+++ more/more.js
@@ -10,12 +10,6 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-
-// TODO: Update the application UI when
-// 		- Installing an application
-// 		- Uninstalling an application
-// 		- Adding a repository
-// 		- Removing a repository
 // TODO: List version installed on app page
 // TODO: Allow extra defined links provided by the metadata
 
