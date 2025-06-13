12d723cb2a56c95e5b4463dc71dfd70c9e019509
armored-dragon <publicmail@armoreddragon.com>
2025-06-13 10:22:15 -0500

    Save notifications.

--- notifications/lib/io.js
+++ notifications/lib/io.js
@@ -1,14 +1,19 @@
+const SETTING_SYSTEM_NOTIFICATION = "overte.notification.system";
+const SETTING_CONNECTION_NOTIFICATION = "overte.notification.connection";
+
 const io = {
 	saveNotification: (messageJSON) => {
 		if (messageJSON.type === "system") {
 			app._data.systemNotifications.push(messageJSON);
 			app._data.systemNotifications = clampArrayLengthToMaximumLength(app._data.systemNotifications, app._config.maximumSavedSystemNotifications)
+			Settings.setValue(SETTING_SYSTEM_NOTIFICATION, app._data.systemNotifications);
 			return;
 		}
 
 		if (messageJSON.type === "connection") {
 			app._data.connectionNotifications.push(messageJSON);
 			app._data.connectionNotifications = clampArrayLengthToMaximumLength(app._data.connectionNotifications, app._config.maximumSavedConnectionNotifications)
+			Settings.setValue(SETTING_CONNECTION_NOTIFICATION, app._data.connectionNotifications);
 			return;
 		}
 
